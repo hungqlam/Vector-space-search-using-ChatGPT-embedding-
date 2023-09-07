3f9b9b23ae0b6da6ab680fa1d7619bbfc919c286
hungqlam <98580130+hungqlam@users.noreply.github.com>
2023-09-07 10:06:09 -0700

    Final version

---
+++ htmlTemplates.py
@@ -0,0 +1,46 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://drive.google.com/uc?id=1lI9KBvZSHc9GXmtWlvo5Xbx3D1sBMydn
+" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://drive.google.com/uc?id=1VpOTWIFIGDLb2sv2QWfOoOkOfbXsu_G6
+">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
